e9647f810bd88c3b55a7e56eab189a5ff415f0fb
Toby Ferguson <toby@cloudera.com>
2017-10-30 11:26:49 -0700

    Minor changes for CDH-5.13, corrected xip back to nip!

--- scripts/java8-bootstrap-script.sh
+++ scripts/java8-bootstrap-script.sh
@@ -20,7 +20,10 @@ exec >~/java8-bootstrap-script.log 2>&1
 # We remove any natively installed JDKs, as both Cloudera Manager and Cloudera Director only support Oracle JDKs
 yum remove --assumeyes *openjdk* oracle-j2sdk1.7*
 
+# Only install/update java if there isn't a java on the path or if the current version isn't 1.8
+which java && java -version 2>&1 | grep -q 1.8.0_121 && { echo "Java 1.8 found. No need to install java"; exit 0; }
 
+echo "Java 1.8 needs to be installed"
 yum -y install http://archive.cloudera.com/director/redhat/7/x86_64/director/2.5.0/RPMS/x86_64/oracle-j2sdk1.8-1.8.0+update121-1.x86_64.rpm
 
 JAVA_HOME=/usr/java/jdk1.8.0_121-cloudera
